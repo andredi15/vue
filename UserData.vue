f5bcfb93095aff6323c119b5587f9772cd39484e
andredi15 <andredi15@gmail.com>
2023-01-17 15:31:08 -0500

    Add files via upload

---
+++ UserData.vue
@@ -0,0 +1,34 @@
+<template>
+    <form @submit.prevent="submitData">
+        <div>
+            <label for="">Username</label>
+            <input v-model="enteredName" type="text">
+            <label for="">Age</label>
+            <input v-model="enteredAge" type="number">
+        </div>
+        <div>
+            <button>Add User</button>
+        </div>
+    </form>
+</template>
+
+<script>
+
+export default {
+    emits:['add-user'],
+    data() {
+        return{
+            enteredName: '',
+            enteredAge: '',
+        }
+    },
+    methods: {
+        submitData() {
+            this.$emit('add-user',this.enteredName, this.enteredAge)
+        }
+    }
+}
+
+
+
+</script>
