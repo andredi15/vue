9882af2d69bbd9bb2da41596bc663c574f5401c8
andredi15 <andredi15@gmail.com>
2023-01-19 04:49:06 -0500

    Add files via upload

---
+++ src/components/ActiveUser.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2>{{ username }}</h2>
+        <h3>{{ age }}</h3>
+    </div>    
+</template>
+
+<script>
+
+export default {
+    props: {
+        id: {
+            type: String,
+            required: true
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        age: {
+            type: Number,  
+            required: true  
+        }
+    }
+}    
+</script>
